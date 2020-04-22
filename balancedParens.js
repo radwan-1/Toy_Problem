7a040bd8fc98e0e8d63b1212ccc54ed6d0d3c019
radwan-1 <radwanbara@gmail.com>
2020-04-22 16:27:59 -0700

    Toy Problem #1 Answer

--- balancedParens.js
+++ balancedParens.js
@@ -23,3 +23,39 @@
  *
  *
  */
+
+var similarParenthesis = function (symbol) {
+    var stack = [];
+    var map = {
+        ')' : '(',
+        ']' : '[',
+        '}' : '{'
+    }
+
+    var length = symbol.length;
+    var i, currentChar, popped;
+
+    for (i = 0; i < length; i += 1) {
+            currentChar = input[i];
+          
+            if ( openParen(currentChar) ) {
+                stack.push( currentChar );
+            } else if ( closeParen(currentChar) ) {
+                popped = stack.pop();
+                if ( map[currentChar] !== popped ) {
+                    return false;
+                }
+            }
+    }
+          
+        return stack.length === 0;
+};
+          
+var openParen = function (char) {
+    return char === '(' || char === '[' || char === '{';
+};
+
+        
+var closeParen = function (char) {
+    return char === ')' || char === ']' || char === '}';
+};
