fcd4ba960028fd9f5d8df921241cc7d8de39c53c
radwan-1 <radwanbara@gmail.com>
2020-05-08 07:31:27 -0700

    Toy Problem #7

---
+++ commonAncestor.js
@@ -0,0 +1,13 @@
+/**
+  * implement the function `getClosestCommonAncestor` and `getAncestorPath`
+  * in the following Tree class
+  */
+
+/** example usage:
+  * var grandma = new Tree('grandma');
+  * var mom = new Tree('mom');
+  * grandma.addChild(mom);
+  * var me = new Tree('me');
+  * mom.addChild(me);
+  * grandma.getAncestorPath(me); // => [grandma, mom, me]
+*/
